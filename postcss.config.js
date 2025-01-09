e6f737cbd2c73557cc3a6f8eafbceb54d1ba4f9e
anasshabrah <syriandealer@gmail.com>
2025-01-10 02:09:56 +0300

    fix issues

--- postcss.config.js
+++ postcss.config.js
@@ -3,39 +3,37 @@
 const purgecss = require('@fullhuman/postcss-purgecss');
 
 module.exports = {
-  plugins: [
-    require('postcss-import')(),
-    require('postcss-preset-env')({
+  plugins: {
+    'postcss-import': {}, // Allows using @import in CSS
+    'postcss-preset-env': { // Enables modern CSS features
       stage: 3,
       features: {
         'nesting-rules': true,
       },
-    }),
-    ...(process.env.NODE_ENV === 'production'
-      ? [
-          purgecss({
-            content: [
-              './app/**/*.{js,jsx,ts,tsx}',
-              './components/**/*.{js,jsx,ts,tsx}',
-              './pages/**/*.{js,jsx,ts,tsx}',
-            ],
-            defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-            safelist: {
-              standard: [
-                'active',
-                'opened',
-                /^nav-/,
-                /^btn-/,
-                /^text-/,
-                /^bg-/,
-                /^swiper/,
-              ],
-            },
-          }),
-          require('cssnano')({
-            preset: 'default',
-          }),
-        ]
-      : []),
-  ],
+    },
+    ...(process.env.NODE_ENV === 'production' ? { // Only include these plugins in production
+      '@fullhuman/postcss-purgecss': { // Removes unused CSS
+        content: [
+          './app/**/*.{js,jsx,ts,tsx}',
+          './components/**/*.{js,jsx,ts,tsx}',
+          './pages/**/*.{js,jsx,ts,tsx}',
+        ],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: { // Prevents specific classes from being purged
+          standard: [
+            'active',
+            'opened',
+            /^nav-/,
+            /^btn-/,
+            /^text-/,
+            /^bg-/,
+            /^swiper/,
+          ],
+        },
+      },
+      'cssnano': { // Minifies CSS
+        preset: 'default',
+      },
+    } : {}),
+  },
 };
